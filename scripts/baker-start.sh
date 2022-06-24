ff88c9246a063b70747b864f4dbbbce9a5242302
Vivienbcr <vivien.boucher@outlook.com>
2022-06-24 08:47:06 +0200

    folder permissions

--- scripts/baker-start.sh
+++ scripts/baker-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Start baking process .. "
+sudo chmod -R 777 /home/tezos/.tezos-client
 sleep 15
 tezos-client --remote-signer http://$SIGNER_URL import public key baker http://$SIGNER_URL/$PKH --force
 tezos-baker-013-PtJakart -R http://$SIGNER_URL --endpoint http://$NODE_URL run with local node /home/tezos/.tezos-node --liquidity-baking-toggle-vote on baker
