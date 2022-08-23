ef05914a51491fb12faea3a36438bdaa58a474bd
Vivien Boucher <vivien.boucher@exaion.com>
2022-08-23 18:33:30 +0200

    ghostnet setup

--- activate-account.sh
+++ activate-account.sh
@@ -8,12 +8,12 @@ if [ ! -f "$FILE" ]; then
     echo "$FILE file does not exist."
 fi
 echo "Activate account, it will take some time..."
-PRIV_KEY=$(docker run --entrypoint /bin/sh -v $(pwd)/$FILE:/temp/faucet.json -e TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y tezos/tezos:v13.0 -c "sudo apk add jq > /dev/null \
-    && tezos-client -E https://rpc.jakartanet.teztnets.xyz activate account faucet with /temp/faucet.json &> /dev/null \
+PRIV_KEY=$(docker run --entrypoint /bin/sh -v $(pwd)/$FILE:/temp/faucet.json -e TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y tezos/tezos:v14.0 -c "sudo apk add jq > /dev/null \
+    && tezos-client -E https://rpc.ghostnet.teztnets.xyz activate account faucet with /temp/faucet.json &> /dev/null \
     && cat /home/tezos/.tezos-client/secret_keys | jq -r '.[] | select(.name==\"faucet\") | .value' ")
 
 echo "Private key exported, wait for public key hash..."
-PUB_KEY=$(docker run --entrypoint /bin/sh -v $(pwd)/$FILE:/temp/faucet.json -e TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y tezos/tezos:v13.0 -c "sudo apk add jq > /dev/null \
+PUB_KEY=$(docker run --entrypoint /bin/sh -v $(pwd)/$FILE:/temp/faucet.json -e TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y tezos/tezos:v14.0 -c "sudo apk add jq > /dev/null \
     && tezos-client import secret key faucet $PRIV_KEY &> /dev/null \
     && cat /home/tezos/.tezos-client/public_key_hashs | jq -r '.[] | select(.name==\"faucet\") | .value' ")
 
@@ -25,8 +25,8 @@ echo "BAKER_PUB_KEY_HASH=$PUB_KEY" >> .env
 
 echo "Set faucet account delegated to self"
 
-docker run --entrypoint /bin/sh -v $(pwd)/$FILE:/temp/faucet.json -e TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y tezos/tezos:v13.0 -c "sudo apk add jq > /dev/null \
+docker run --entrypoint /bin/sh -v $(pwd)/$FILE:/temp/faucet.json -e TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y tezos/tezos:v14.0 -c "sudo apk add jq > /dev/null \
     && tezos-client import secret key faucet $PRIV_KEY &> /dev/null \
-    && tezos-client --endpoint https://rpc.jakartanet.teztnets.xyz register key faucet as delegate"
+    && tezos-client --endpoint https://rpc.ghostnet.teztnets.xyz register key faucet as delegate"
 
 echo "Done."
--- scripts/node-start.sh
+++ scripts/node-start.sh
@@ -2,14 +2,15 @@
 echo "Take ownership of mounted path (dev purpose 777)"
 sudo chmod -R 777 /mnt
 sudo chmod -R 777 $TEZOS_HOME_CONFIG
-
+echo $TEZOS_HOME_CONFIG
 if [ ! -f "$ID_FILE" ] ; then
     echo "Identity File not found :  Init configuration..."
-    rm -f $TEZOS_HOME_CONFIG/config.json
-    tezos-node config init --data-dir=$TEZOS_HOME_CONFIG --history-mode rolling --network=jakartanet --net-addr=0.0.0.0:$P2P_PORT --rpc-addr=0.0.0.0:$RPC_PORT --allow-all-rpc=0.0.0.0:$RPC_PORT
+    rm -rf $TEZOS_HOME_CONFIG
+    tezos-node config init --data-dir=$TEZOS_HOME_CONFIG --history-mode rolling --network=https://teztnets.xyz/ghostnet --net-addr=0.0.0.0:$P2P_PORT --rpc-addr=0.0.0.0:$RPC_PORT --allow-all-rpc=0.0.0.0:$RPC_PORT
+    if [ $USE_SNAPSHOT = "true" ] ; then
     echo "Get snapshot for testnet"
-    if [ $USE_SNAPSHOT = "true"]; then
-        echo "TODO: Snapshot unavailable yet for jakartanet"
+        wget https://ghostnet.xtz-shots.io/rolling -O tezos-ghostnet.rolling
+        tezos-node snapshot import tezos-ghostnet.rolling --data-dir=$TEZOS_HOME_CONFIG
     fi
 fi
 echo "Run tezos node"
